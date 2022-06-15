8191b4754d6cdc05000b61995d1fe6376494ca1a
SrdjanLakic <srdjan949@gmail.com>
2022-06-15 10:49:11 -0400

    getData function added

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const playerScore = document.querySelector('span');
 const score = 0;
 
 playerScore.textContent = score;
+
+const getData = () => [
+  { imgSrc: './img/bulls.png', name: 'bulls' },
+  { imgSrc: './img/celtics.png', name: 'celtics' },
+  { imgSrc: './img/heat.png', name: 'heat' },
+  { imgSrc: './img/knicks.png', name: 'knicks' },
+  { imgSrc: './img/lakers.png', name: 'lakers' },
+  { imgSrc: './img/mavericks.png', name: 'mavericks' },
+  { imgSrc: './img/nuggets.png', name: 'nuggets' },
+  { imgSrc: './img/spurs.png', name: 'spurs' },
+  { imgSrc: './img/bulls.png', name: 'bulls' },
+  { imgSrc: './img/celtics.png', name: 'celtics' },
+  { imgSrc: './img/heat.png', name: 'heat' },
+  { imgSrc: './img/knicks.png', name: 'knicks' },
+  { imgSrc: './img/lakers.png', name: 'lakers' },
+  { imgSrc: './img/mavericks.png', name: 'mavericks' },
+  { imgSrc: './img/nuggets.png', name: 'nuggets' },
+  { imgSrc: './img/spurs.png', name: 'spurs' },
+];
